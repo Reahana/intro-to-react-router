c63a9ebac6589d56ccafd6e4e1bfd908ffd020b9
Reahana <reahana233@gmail.com>
2024-01-21 20:55:35 +0600

    friend detail

---
+++ src/Components/FriendDetail/FriendDetail.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const FriendDetail = () => {
+    return (
+        <div>
+            <h3>Everything about this person is here</h3>
+        </div>
+    );
+};
+
+export default FriendDetail;
