3933d91d4698bc63aca93ff258e5bee17684e709
Reahana <reahana233@gmail.com>
2024-01-21 21:02:22 +0600

    friend detail

--- src/Components/FriendDetail/FriendDetail.jsx
+++ src/Components/FriendDetail/FriendDetail.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const FriendDetail = () => {
+    const friend = useLoaderData();
     return (
         <div>
             <h3>Everything about this person is here</h3>
+            <h2>Name: {friend.name}</h2>
+            <h3>Phone: {friend.phone}</h3>
         </div>
     );
 };
