dab415fad59b76d655b0b7fef9f6ed703f70cf53
Reahana <reahana233@gmail.com>
2024-01-21 23:17:23 +0600

    post

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Post = ({post}) => {
      const {id, title, body}= post;
     return (
         <div>
             <h2>{title}</h2>
-            <h3>{body}</h3>
+            <p><Link to= {`/post/${id}`}>Read... </Link></p>
+
         </div>
     );
 };
