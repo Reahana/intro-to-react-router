17b125a3e6600a11e005e145a8d4dd172c1ecc92
Reahana <reahana233@gmail.com>
2024-01-21 21:05:50 +0600

    posts

---
+++ src/Components/Posts/Posts.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Posts = () => {
+    return (
+        <div>
+            <h1>all posts are here</h1>
+        </div>
+    );
+};
+
+export default Posts;
