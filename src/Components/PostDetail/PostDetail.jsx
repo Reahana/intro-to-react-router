f8a52ed70171f97e5d3f861c7ae3a772b762e125
Reahana <reahana233@gmail.com>
2024-01-21 23:37:12 +0600

    all

--- src/Components/PostDetail/PostDetail.jsx
+++ src/Components/PostDetail/PostDetail.jsx
@@ -1,16 +1,25 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 
 const PostDetail = () => {
     const post =useLoaderData()
     return (
-        <div>
-          <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <h4>author:{post.userId}</h4>
-        </div>
+      
+           <Card>
+            <Card.Body>
+              <Card.Title>{post.title}</Card.Title>
+              <Card.Text>
+              {post.body}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+          <small className="text-muted">author:{post.userId}</small>
+        </Card.Footer>
+          </Card>
+        
     );
 };
 
