9056a0bda373b033597966c7cca894ffde09b90c
Reahana <reahana233@gmail.com>
2024-01-21 11:53:14 +0600

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,21 +9,40 @@ import {
 import './index.css'
 import About from './Components/About/About.jsx';
 import Contact from './Components/Contact/Contact.jsx';
+import Home from './Components/Home/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <Home></Home>,
+    children:[
+      {
+        path: "about",
+        element: <About></About>,
+      },
+      {
+             path: "contact",
+             element: <Contact></Contact>,
+       },
+    ]
   },
-  {
-    path: "/about",
-    element: <About></About>,
-  },
-  {
-    path: "/contact",
-    element: <Contact></Contact>,
-  },
+  
 ]);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>,
+//   },
+//   {
+//     path: "/about",
+//     element: <About></About>,
+//   },
+//   {
+//     path: "/contact",
+//     element: <Contact></Contact>,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
