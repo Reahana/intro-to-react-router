eb9cd0778d480b5a8b121c9a6b3d6c81bfa8dc41
Reahana <reahana233@gmail.com>
2024-01-21 20:45:39 +0600

    Friends

--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const friends = useLoaderData();
+   
     return (
         <div>
-            
+            <h2>This are my friends : {friends.length}</h2>
+        <div>
+            {
+                friends.map(friend => <Friend
+                key={friend.id}
+                friend ={friend}
+                ></Friend>)           
+             }
+        </div>
         </div>
     );
 };
