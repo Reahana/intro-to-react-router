456098dc27cc96afcf3f9e5907782008e3f05d7f
Reahana <reahana233@gmail.com>
2024-01-22 12:04:46 +0600

    button

--- src/Components/Friend/Friend.jsx
+++ src/Components/Friend/Friend.jsx
@@ -2,16 +2,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './Friend.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Friend = ({friend}) => {
-    const {email, name, id , phone}= friend
+    const {email, name, id , phone}= friend;
+    const navigate = useNavigate();
+const handleNavigation = () =>{
+    navigate(`/friend/${id}`)
+}
+
     return (
         <div className='friend'> 
             <h2>{name}</h2>
             <h3>email: {email}</h3>
             <h3>phone: {phone}</h3>
             <p><Link to= {`/friend/${id}`}>Show me </Link></p>
+            <Button className='bg-danger' onClick={handleNavigation}>Details</Button>
 
         </div>
     );
--- src/Components/FriendDetail/FriendDetail.jsx
+++ src/Components/FriendDetail/FriendDetail.jsx
@@ -1,14 +1,20 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const FriendDetail = () => {
     const friend = useLoaderData();
+    const navigate = useNavigate()
+    const handleGoBack =() =>{
+navigate(-1);
+    }
     return (
         <div>
             <h3>Everything about this person is here</h3>
             <h2>Name: {friend.name}</h2>
             <h3>Phone: {friend.phone}</h3>
+            <Button className='bg-dark' onClick={handleGoBack}>Go back</Button>
         </div>
     );
 };
