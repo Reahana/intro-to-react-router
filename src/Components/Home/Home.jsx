c686994b1bb74a3441071ca60534e3f8ece7caf7
Reahana <reahana233@gmail.com>
2024-02-11 20:37:47 +0600

    home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
+           {
+                navigation.state === "loading" ? 
+                <p>Loading...</p>:''
+               
+            }
             <Outlet></Outlet>
         </div>
     );
